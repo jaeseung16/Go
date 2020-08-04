1416b05a13f8a934686302603f769f16f1d0ff56
Jae-Seung Lee <jaeseung@gmail.com>
2020-08-04 19:17:43 -0400

    Initial Commit

---
+++ Go/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Go
+//
+//  Created by Jae Seung Lee on 8/4/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
