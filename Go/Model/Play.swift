f441d48e186130abe6f6012c6a311d46ce57dd08
Jae-Seung Lee <jaeseung@gmail.com>
2020-08-18 18:55:43 -0400

    Add Play.swift

---
+++ Go/Model/Play.swift
@@ -0,0 +1,17 @@
+//
+//  Play.swift
+//  Go
+//
+//  Created by Jae Seung Lee on 8/18/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Play {
+    var id: Int
+    var row: Int
+    var column: Int
+    var stone: Stone
+    
+}
