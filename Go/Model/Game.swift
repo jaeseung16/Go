f08d5f3f013b370ea5975bd4c4ad1a5a198c828b
Jae-Seung Lee <jaeseung@gmail.com>
2020-09-05 18:40:59 -0400

    Create Game.swift

---
+++ Go/Model/Game.swift
@@ -0,0 +1,28 @@
+//
+//  Game.swift
+//  Go
+//
+//  Created by Jae Seung Lee on 8/15/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    // Players
+    let players = [Player(stone: .Black, score: 0), Player(stone: .White, score: 0)]
+    
+    // Plays
+    // play number, stone, coordinate, time
+    
+    // Rule
+    // Korean, Chinese, Japan, and so on
+    // Komi 5.5, 6.5, 7.5
+    // Handicap: 1-9
+    // How to end
+    // Score
+    
+    // Clock
+    
+}
