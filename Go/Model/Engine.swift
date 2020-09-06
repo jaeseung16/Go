bf78f5370a9d8f0f2a2d61967a6714877b908ce4
Jae-Seung Lee <jaeseung@gmail.com>
2020-09-06 15:07:20 -0400

    Create EngineDelegate.switf

---
+++ Go/Model/Engine.swift
@@ -0,0 +1,13 @@
+//
+//  Engine.swift
+//  Go
+//
+//  Created by Jae Seung Lee on 9/6/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+protocol EngineDelegate {
+    func read(result: String)
+}
