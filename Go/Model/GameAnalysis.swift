4e09a7607b32c5cdb29b09df9e4c616276c2c7af
Jae-Seung Lee <jaeseung@gmail.com>
2020-09-07 11:15:33 -0400

    Create GameAnalysis.swift

---
+++ Go/Model/GameAnalysis.swift
@@ -0,0 +1,16 @@
+//
+//  GameAnalysis.swift
+//  Go
+//
+//  Created by Jae Seung Lee on 9/7/20.
+//  Copyright © 2020 Jae Seung Lee. All rights reserved.
+//
+
+import Foundation
+
+struct GameAnalysis {
+    var playMade: Play
+    var bestNextPlay: Play
+    var winrate: Double
+    var scoreLead: Double
+}
