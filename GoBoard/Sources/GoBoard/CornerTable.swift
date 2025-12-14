730b92686df448e6623656a63c2cb43cd8487c98
Jae-Seung Lee <jaeseung@gmail.com>
2025-12-14 11:14:50 -0500

    feat: Add GoString

---
+++ GoBoard/Sources/GoBoard/CornerTable.swift
@@ -0,0 +1,24 @@
+//
+//  CornorTable.swift
+//  GoBoard
+//
+//  Created by Jae Seung Lee on 12/7/25.
+//
+
+struct CornorTable {
+    
+    var dimension: Int
+    var table: [Point: [Point]]
+    
+    init(dimension: Int) {
+        for row in 1...dimension {
+            for col in 1...dimension {
+                let point = Point(row: row, col: col)
+                let trueCorners = point.corners.filter {
+                    1...dimension ~= $0.row && 1...dimension ~= $0.col
+                }
+                table[point] = trueCorners
+            }
+        }
+    }
+}
---
+++ GoBoard/Sources/GoBoard/GoString.swift
@@ -0,0 +1,37 @@
+//
+//  GoString.swift
+//  GoBoard
+//
+//  Created by Jae Seung Lee on 12/14/25.
+//
+
+public struct GoString: Equatable {
+    
+    public var color: Stone
+    public var stones: Set<Point>
+    public var liberties: Set<Point>
+    
+    public var numberOfLibierties: Int {
+        return liberties.count
+    }
+    
+    public func without(liberty: Point) -> GoString {
+        let newLiberties = liberties.subtracting([liberty])
+        return GoString(color: color, stones: stones, liberties: newLiberties)
+    }
+    
+    public func with(liberty: Point) -> GoString {
+        let newLiberties = liberties.union([liberty])
+        return GoString(color: color, stones: stones, liberties: newLiberties)
+    }
+    
+    public func merged(with other: GoString) -> GoString {
+        guard self.color == other.color else {
+            fatalError("Can't merge strings of different colors.")
+        }
+        let newStones = stones.union(other.stones)
+        let newLiberties = liberties.union(other.liberties).subtracting(newStones)
+        return GoString(color: color, stones: newStones, liberties: newLiberties)
+    }
+    
+}
