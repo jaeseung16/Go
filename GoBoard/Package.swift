27a582ce9d8457d98fc75c46e7be9e8ac707973d
Jae-Seung Lee <jaeseung@gmail.com>
2025-12-21 14:23:51 -0500

    chore: Add Package.swift

---
+++ GoBoard/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GoBoard",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "GoBoard",
+            targets: ["GoBoard"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log", from: "1.6.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "GoBoard",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+            ]
+        ),
+        .testTarget(
+            name: "GoBoardTests",
+            dependencies: ["GoBoard"]
+        ),
+    ]
+)
