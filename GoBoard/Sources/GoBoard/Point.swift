ea0cb934bb85e9b696689ba1a12b6e90b8d4cc64
Jae-Seung Lee <jaeseung@gmail.com>
2025-12-21 14:22:35 -0500

    feat: Add neighbors and corners to Point

--- GoBoard/Sources/GoBoard/Point.swift
+++ GoBoard/Sources/GoBoard/Point.swift
@@ -6,6 +6,24 @@
 //
 
 public struct Point: Equatable, Hashable, Sendable {
-    var row: Int
-    var col: Int
+    public var row: Int
+    public var col: Int
+    
+    public var neighbors: [Point] {
+        [
+            Point(row: row - 1, col: col),
+            Point(row: row + 1, col: col),
+            Point(row: row, col: col - 1),
+            Point(row: row, col: col + 1)
+        ]
+    }
+    
+    public var corners: [Point] {
+        [
+            Point(row: row - 1, col: col - 1),
+            Point(row: row - 1, col: col + 1),
+            Point(row: row + 1, col: col - 1),
+            Point(row: row + 1, col: col + 1)
+        ]
+    }
 }
