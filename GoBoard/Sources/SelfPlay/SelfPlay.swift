070de674f3b2ded5654cab1251cb2a42ea0c8130
Jae-Seung Lee <jaeseung@gmail.com>
2026-08-02 15:28:23 -0400

    feat: Collect experiences while self-playing

--- GoBoard/Sources/SelfPlay/SelfPlay.swift
+++ GoBoard/Sources/SelfPlay/SelfPlay.swift
@@ -32,7 +32,7 @@ struct SelfPlay: ParsableCommand {
     var learningAgent: String = "policy"
     
     @Option(help: "The path to save the experience")
-    var experienceOut: String = "experience.pkl"
+    var experienceOut: String = "experience.safetensors"
 
     mutating func run() throws {
         let logURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
@@ -54,19 +54,60 @@ struct SelfPlay: ParsableCommand {
         
         let board = ZobristGoBoard(dimension: self.boardSize)
         
-        let whitePlayer = createPlayer(for: .white)
-        let blackPlayer = createPlayer(for: .black)
+        var whitePlayer = createPlayer(for: .white)
+        var blackPlayer = createPlayer(for: .black)
         let players: [Player: GoAgent] = [.white: whitePlayer, .black: blackPlayer]
         
-        for _ in 0..<self.numGames {
-            print("Game #\(self.numGames)")
+        let experienceCollector1 = ExperienceCollector()
+        let experienceCollector2 = ExperienceCollector()
+        
+        var gameCount = 0
+        while gameCount < self.numGames {
+            if gameCount % 2 == 0 {
+                whitePlayer.experienceCollector = experienceCollector1
+                blackPlayer.experienceCollector = experienceCollector2
+            } else {
+                whitePlayer.experienceCollector = experienceCollector2
+                blackPlayer.experienceCollector = experienceCollector1
+            }
+            
+            experienceCollector1.beginEpisode()
+            experienceCollector2.beginEpisode()
+            
+            print("Starting Game #\(gameCount)")
             var game = GameState(board: board, nextPlayer: .black)
             
             while !game.isOver() {
                 let move = players[game.nextPlayer]!.select(from: game)
                 game = game.apply(move: move)
             }
+            
+            let score = ScoringHelper(gameState: game).compute()
+            print("score=\(score)")
+            
+            print("Finished Game #\(gameCount): Winner=\(String(describing: game.winner)), remainingLegalMoves=\(game.legalMoves)")
+            
+            if game.winner == .black {
+                blackPlayer.experienceCollector?.completeEpisode(reward: MLXArray(1.0))
+                whitePlayer.experienceCollector?.completeEpisode(reward: MLXArray(-1.0))
+            } else {
+                blackPlayer.experienceCollector?.completeEpisode(reward: MLXArray(-1.0))
+                whitePlayer.experienceCollector?.completeEpisode(reward: MLXArray(1.0))
+            }
+            
+            print("Collected \(experienceCollector1.states.count + experienceCollector2.states.count) experiences")
+            
+            gameCount += 1
         }
+        
+        let experienceBuffer = ExperienceBuffer.from(experiences: [experienceCollector1, experienceCollector2])
+        
+        let url = URL(fileURLWithPath: self.experienceOut)
+        print("Saving experiences to \(url.path)")
+        experienceBuffer.save(to: url)
+        
+        
+        print("Collected \(experienceBuffer.states.shape[0]) experiences")
         
         print("FINISHED")
    
